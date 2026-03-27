2c4266b31c074c6129d914ea13673d3fe1beee2c
CLEYTON MONTEIRO CARVALHO <cleyton30@gmail.com>
2026-03-27 17:01:12 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,3 @@
 self.addEventListener('install', (event) => {
     console.log('Service Worker instalado');
 });
-
-self.addEventListener('fetch', (event) => {
-    // Permite que o PWA passe pela verificação do navegador
-});
